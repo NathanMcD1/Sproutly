e8c0cd749f0d97463dc30149611f5f58d98685d5
NathanMcD1 <131803912+NathanMcD1@users.noreply.github.com>
2025-04-23 13:18:51 -0400

    Add files via upload

--- Sproutly/Models/Account.java
+++ Sproutly/Models/Account.java
@@ -1,5 +1,7 @@
 package Sproutly.Models;
 
+import java.sql.*;
+
 public class Account{
    private String username;
    private String password;
@@ -7,6 +9,7 @@ public class Account{
    private int goalTarget;
    private int goalCurrent;
    private boolean isNew;
+   private boolean update;
    
    public Account(){
       username = "";
@@ -15,6 +18,30 @@ public class Account{
       goalTarget = 0;
       goalCurrent = 0;
       isNew = true;
+      update = false;
+   }
+   
+   public void setAccount() throws Exception{
+      String sql = "SELECT password, goal, goalTarget, goalCurrent FROM users WHERE username = '" + username + "'";
+      Connection connection = DriverManager.getConnection("jdbc:sqlite:Sproutly\\DataBase\\SproutlyData.db");
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+         password = rs.getString("password");
+         goal = rs.getString("goal");
+         goalTarget = rs.getInt("goalTarget");
+         goalCurrent = rs.getInt("goalCurrent");
+         isNew = false;
+      }
+      ps.close();
+      connection.close();
+   }
+   
+   public void setUpdate(boolean update){
+      this.update = update;
+   }
+   public boolean getUpdate(){
+      return update;
    }
    
    public void setUsername(String username){
@@ -27,7 +54,7 @@ public class Account{
    public void setPassword(String password){
       this.password = password;
    }
-   public String getPassword(String username){
+   public String getPassword(){
       return password;
    }
    
