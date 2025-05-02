e515fe04fd1a3d839a8f9ef4ffdf5b49293ba6c0
NathanMcD1 <131803912+NathanMcD1@users.noreply.github.com>
2025-05-02 08:30:04 -0400

    Update UserAware.java

--- Sproutly/Controllers/UserAware.java
+++ Sproutly/Controllers/UserAware.java
@@ -2,6 +2,11 @@ package Sproutly.Controllers;
 
 import Sproutly.Models.Account;
 
+/**
+ * Interface for controllers that need access to the current user account.
+ * Enables shared Account injection across scenes.
+ */
+
 public interface UserAware {
     void setUser(Account user);
 }
